82763dfe8be9872503b1ee5950cf827b36f1962f
Leyukezer_Ifro <2022202060022@ifro.aluno>
2022-09-29 22:03:43 -0400

    atualização

--- ExercicioFixacao/Atividade4.4/Program.cs
+++ ExercicioFixacao/Atividade4.4/Program.cs
@@ -1,1 +1,25 @@
-﻿//
+﻿//calcule o salario reajustado de um funcionario, exibindo como resultado
+//seu nome, o valor do salário atual e o valor do salário reajustado
+
+//dados
+string nome;
+double salario_atual, salario_final;
+
+//entrada
+Console.WriteLine("---- Reajuste de Salário ----");
+Console.Write("Digite o nome do Funcionario: ");
+nome = Console.ReadLine();
+Console.Write("Digite o Salário Atual do Funcionario: R$");
+salario_atual = Convert.ToDouble(Console.ReadLine());
+
+//processamento
+
+salario_final = salario_atual * 1.0875;
+
+//saida
+Console.WriteLine("\n\n------ Loreite do Funcionario ------\n" +
+                  "#Nome do Funcionario: "+nome+"\n" +
+                  "#Salario mensal: R$"+salario_atual+"\n" +
+                  "## Salario Reajustado: R$"+salario_final);
+
+Console.ReadKey();
---
+++ ExercicioFixacao/Atividade4.5/Program.cs
@@ -0,0 +1,21 @@
+﻿//10% de desconto
+//faça um algoritimo que, a partir do valor fornecido,
+// calcule e exiba o preço atual e o preço com desconto
+
+//dados
+double valor_produto, vdesconto_produto;
+
+//entrada
+Console.WriteLine("---- Desconto no valor do Produto ----");
+Console.Write("Digite o preço do produto: R$");
+valor_produto = Convert.ToDouble(Console.ReadLine());
+
+//processamento
+vdesconto_produto = valor_produto - (valor_produto * 0.1);
+
+//saida
+Console.WriteLine("\n\n---- Desconto do Produto ----\n" +
+                  "# Preço Inicial: R$" + valor_produto+ "\n" +
+                  "# Preço com Desconto: R$" + vdesconto_produto);
+
+Console.ReadKey();
