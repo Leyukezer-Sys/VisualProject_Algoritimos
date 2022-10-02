0ed33e182153e5186d0e707da111b8bb2905d7d0
LeyukezerPc <leyukezer@gmail.com>
2022-10-01 22:31:50 -0400

    Atualização 1-10-22

---
+++ ExercicioFixacao/Atividade4.10/Program.cs
@@ -0,0 +1,17 @@
+﻿// faça um algoritimo que leia o valor total de uma compra
+// e calcule o valor do pagamento final de acordo com a opção escolhida,
+// Se for pagamento parcelado, calcule tambem o valor da parcela.
+//ao final, apresente o valor total e o valor das parcelas;
+
+//dados
+double preco = 0, pagamentofinal = 0;
+
+//entrada
+Console.WriteLine("--- Formas de Pagamentos ---");
+Console.Write("Valor do produto: R$");
+preco = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Escolha sua forma de pagamento:" +
+                  "# 101 - Pagamento à vista!" +
+                  "# 202 - pagamento com 3 parcelas (aprox R$"+(preco/3)+")" +
+                  "# 203 - pagamento com 5 parcelas (aprox R$"+(preco/5)+ " + 2% de Juros)" +
+                  "# 204 - pagamento com 10 parcelas (aprox R$"+(preco/10)+ " + 8% de Juros)");
---
+++ ExercicioFixacao/Atividade4.7/Program.cs
@@ -0,0 +1,30 @@
+﻿// Melhore o algoritimo do exercício anterior verificando se o
+//número inserido pelo usuario é zero, par ou impar
+
+//dados
+double nu;
+
+//entrada
+Console.WriteLine("---- Verificar número (par/impar) ----");
+Console.Write("Digite um número para verificação: ");
+nu = Convert.ToDouble(Console.ReadLine());
+
+//processamento
+if (nu == 0)
+{
+    Console.WriteLine("\n---- Resposta ----\n" +
+                      "O número " + nu + " é\n" +
+                      "um Número Neutro");
+}
+else if (nu % 2 == 0)
+{
+    Console.WriteLine("\n---- Resposta ----\n" +
+                      "O número " + nu + " é\n" +
+                      "um Número Par");
+}
+else
+{
+    Console.WriteLine("\n---- Resposta ----\n" +
+                      "O número " + nu + " é\n" +
+                      "um Número ímpar");
+}
