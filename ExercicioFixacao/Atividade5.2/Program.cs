58bd7a4de20a4dca6454a6f20367bc4af9e747b3
Leyukezer_Ifro <2022202060022@ifro.aluno>
2022-11-09 20:41:33 -0400

    atualizacao0911

--- ExercicioFixacao/Atividade5.2/Program.cs
+++ ExercicioFixacao/Atividade5.2/Program.cs
@@ -1,19 +1,20 @@
 ﻿// pedro tem 1,50m e cresce 2cm por ano, enquanto lucas 1,10m e cresce 3cm por ano
 
-double idadePedro = 1.50, idadeLucas = 1.10;
+double idadePedro = 150, idadeLucas = 110;
 string statusA = "", statusB = "";
 int ano = 0;
 
-while (idadePedro > idadeLucas)
+do
 {
-    idadePedro = idadePedro + 0.02;
-    idadeLucas = idadeLucas + 0.03;
+    idadePedro = idadePedro + 002;
+    idadeLucas = idadeLucas + 003;
     ano++;
 
     if (idadePedro == idadeLucas)
     {
-        statusA = "a) em "+ano+" ano(s) Lucas e Pedro terão a mesma altura.";
-    }else
+        statusA = "a) em " + ano + " ano(s) Lucas e Pedro terão a mesma altura.";
+    }
+    else
     {
         statusA = "a) Não é possivel terem a mesma altura!";
     }
@@ -25,6 +26,6 @@ while (idadePedro > idadeLucas)
     {
         statusB = "b) Não é possivel o Lucar ser mais alto que Pedro!";
     }
-}
+} while (idadePedro >= idadeLucas);
 
 Console.WriteLine(statusA+"\n"+statusB);
