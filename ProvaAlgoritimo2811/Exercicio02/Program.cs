fa9d27ee7612f68e81df58b5393fc6af348b2196
Leyukezer_Ifro <2022202060022@ifro.aluno>
2022-11-28 20:15:38 -0400

    ok

---
+++ ProvaAlgoritimo2811/Exercicio02/Program.cs
@@ -0,0 +1,14 @@
+﻿// Tire a média dos números de 1 a 100 a imprima
+// o valor da média.
+int soma = 0;
+double media;
+for (int i = 0; i <= 100; i++)
+{
+    soma += i;
+}
+
+media = (double)soma / 100;
+
+Console.WriteLine($@"Exercicio 02
+
+Média dos Números de 1 a 100 é {media}");
---
+++ ProvaAlgoritimo2811/Exercicio03/Program.cs
@@ -0,0 +1,26 @@
+﻿// Faça um algoritmo que leia um número inteiro e mostre uma
+//mensagem indicando se este número é par ou ímpar, e se é
+//positivo ou negativo
+
+int valor;
+
+Console.Write(@"Exercicio 03
+
+Digite um valor para verificação: ");
+valor = Convert.ToInt32(Console.ReadLine());
+
+if (valor != 0)
+{
+    if (valor > 0)
+    {
+        if (valor%2 ==0)
+        {
+            Console.WriteLine("O valor digitado é Par e Positivo!");
+        }else Console.WriteLine("O valor digitado é Ímpar e Positivo!");
+    }else if (valor % 2 == 0)
+    {
+        Console.WriteLine("O valor digitado é Par e Negativo!");
+    }
+    else Console.WriteLine("O valor digitado é Ímpar e Negativo!");
+}
+else Console.WriteLine("O valor digitado é Neutro");
