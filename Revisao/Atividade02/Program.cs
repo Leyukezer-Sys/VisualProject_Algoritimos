f83e4ac8b641e5e9456979115ab27d57e54f1bb8
Leyukezer-Labin3_pc10 <2022202060022@JIPA-LABIN03-10>
2023-02-27 20:38:20 -0400

    27022023

--- Revisao/Atividade02/Program.cs
+++ Revisao/Atividade02/Program.cs
@@ -1,1 +1,30 @@
-﻿// Some os números de 1 a 100 a imprima o valor. (10)
+﻿class Exercicio_2
+{
+    static void Main()
+    {
+        //Crie um programa que peça 10 números inteiros e apresente: a média, a soma e o menor. (10)
+
+        int number;
+        double media, soma = 0, menor = 0;
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write($"Insira o {i + 1}º Valor: ");
+            number = Convert.ToInt32(Console.ReadLine());
+            soma += number;
+            if (i == 0) menor = number;
+
+            if (number < menor)
+            {
+                menor = number;
+            }
+
+        }
+        media = soma / 10;
+        Console.WriteLine($"Resultado:\n" +
+                          $"Média: {media}\n" +
+                          $"Soma: {soma}\n" +
+                          $"menor: {menor}");
+    }
+
+}
--- Revisao/Program.cs
+++ Revisao/Program.cs
@@ -1,1 +1,32 @@
-﻿//Crie um programa que peça 10 números inteiros e apresente: a média, a soma e o menor. (10)
+﻿using Microsoft.Win32.SafeHandles;
+
+class Exercicio_1
+{
+    static void Main() 
+    {
+        //Crie um programa que peça 10 números inteiros e apresente: a média, a soma e o menor. (10)
+
+        int number;
+        double media, soma = 0, menor = 0;
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write($"Insira o {i + 1}º Valor: ");
+            number = Convert.ToInt32(Console.ReadLine());
+            soma += number;
+            if (i == 0) menor = number;
+
+            if (number < menor)
+            {
+                menor = number;
+            }
+
+        }
+        media = soma / 10;
+        Console.WriteLine($"Resultado:\n" +
+                          $"Média: {media}\n" +
+                          $"Soma: {soma}\n" +
+                          $"menor: {menor}");
+    }
+    
+}
