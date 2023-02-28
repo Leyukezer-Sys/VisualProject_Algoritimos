b9c8fb57321cfcc3d40e486579a363181bd54940
Leyukezer-Labin3_pc10 <2022202060022@JIPA-LABIN03-10>
2023-02-27 22:18:17 -0400

    27022023

---
+++ Aula27022023/Program.cs
@@ -0,0 +1,13 @@
+﻿// Aula algoritimos 2
+Console.WriteLine("Clayton FERRAZ");
+Double Soma(double num1,double num2)
+{
+    
+
+    return num1+num2;
+}
+void Calcular()
+{
+
+}
+double reesult = Soma(2,5);
