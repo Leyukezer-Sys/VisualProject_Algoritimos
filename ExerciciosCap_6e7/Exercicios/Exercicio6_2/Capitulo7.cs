31a20e4f5f59963d5344e240f03add74febbfbaa
LeyukezerPc <leyukezer@gmail.com>
2023-03-31 00:07:26 -0400

    ok

--- ExerciciosCap_6e7/Exercicios/Exercicio6_2/Capitulo7.cs
+++ ExerciciosCap_6e7/Exercicios/Exercicio6_2/Capitulo7.cs
@@ -74,10 +74,77 @@ namespace Exercicios
              */
 
             string continua = "";
+            int[] matrizA,matrizPar,matrizCinco;
+            int contPar=0, contCinco=0;
             do
             {
                 Console.WriteLine("\n- Exercicio 02 - Capítulo 7\n");
-
+                Console.WriteLine("Quantos elementos serão preenchidos? (ex: 10)");
+                Console.Write("R: ");
+                int eleme = int.Parse(Console.ReadLine());
+                matrizA = new int[eleme];
+                Console.WriteLine("");
+                for (int i = 0; i < matrizA.Length; i++)
+                {
+                    Console.Write($"Digite o {i + 1}º Valor da Matriz: ");
+                    matrizA[i] = int.Parse(Console.ReadLine());
+                    if (matrizA[i] % 2 == 0)
+                    {
+                        contPar++;
+                    }
+                    if (matrizA[i] % 5 == 0)
+                    {
+                        contCinco++;
+                    }
+                }
+                do
+                {
+                    Console.WriteLine("---- Menu ----\n" +
+                                      "digite 1 -> Mostrar os números pares da matriz\n" +
+                                      "digite 2 -> Mostrar os números multiplos de 5 da matriz");
+                    Console.Write("R: ");
+                    int op = int.Parse(Console.ReadLine());
+                    if (op == 1)
+                    {
+                        matrizPar = new int[contPar];
+                        int indice = 0;
+                        for (int i = 0; i < matrizA.Length; i++)
+                        {
+                            if (matrizA[i] % 2 == 0)
+                            {
+                                matrizPar[indice] = matrizA[i];
+                                indice++;
+                            }
+                        }
+                        Console.Write("\nMatriz Numeros Pares: ");
+                        foreach (int item in matrizPar)
+                        {
+                            Console.Write($"|{item}");
+                        }
+                    }else
+                    if (op == 2)
+                    {
+                        matrizCinco = new int[contCinco];
+                        int indice = 0;
+                        for (int i = 0; i < matrizA.Length; i++)
+                        {
+                            if (matrizA[i] % 5 == 0)
+                            {
+                                matrizCinco[indice] = matrizA[i];
+                                indice++;
+                            }
+                        }
+                        Console.Write("\nMatriz Multiplos de 5: ");
+                        foreach (int item in matrizCinco)
+                        {
+                            Console.Write($"|{item}");
+                        }
+                    }
+                    else    Console.WriteLine("Não identificado!");
+                    
+                    Console.WriteLine("\nDeseja voltar ao Menu? (Sim,Nao)(S,N)");
+                    continua = Console.ReadLine().ToUpper();
+                } while (continua.Equals("SIM") || continua.Equals("S"));
 
 
                 Console.WriteLine("*Deseja Refazer o exercício? (Sim,Nao)(S,N)");
@@ -87,7 +154,7 @@ namespace Exercicios
         public void Exercicio3()
         {
             /*
-             Elabore um algoritmo que seja capaz de fa- zer a conversão de um valor em real, 
+             Elabore um algoritmo que seja capaz de fazer a conversão de um valor em real, 
             para o correspondente em dólar, libra, franco e iene, e vice-versa. 
             O programa deverá conter um menu com as opçóes e retornar os resultados para o programa principal, 
             encarregado da exibição dos resultados.
