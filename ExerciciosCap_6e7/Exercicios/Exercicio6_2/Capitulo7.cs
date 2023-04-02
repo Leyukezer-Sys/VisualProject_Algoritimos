231a9fd2a0c3153b6e42a26df3c1851f0612fb97
LeyukezerPc <leyukezer@gmail.com>
2023-04-02 00:07:02 -0400

    ok

--- ExerciciosCap_6e7/Exercicios/Exercicio6_2/Capitulo7.cs
+++ ExerciciosCap_6e7/Exercicios/Exercicio6_2/Capitulo7.cs
@@ -161,10 +161,84 @@ namespace Exercicios
              */
 
             string continua = "";
+            double real = 0.0, dolar = 0.0, libra = 0.0,franco = 0.0,iene = 0.0;
             do
             {
                 Console.WriteLine("\n- Exercicio 03 - Capítulo 7\n");
+                Console.WriteLine("- MENU Conversões -\n" +
+                                  "* Qual a sua moeda que deseja converter?\n" +
+                                  "+Digite 1 -> Real(R$)\n" +
+                                  "+Digite 2 -> Dólar($)\n" +
+                                  "+Digite 3 -> Libra(£)\n" +
+                                  "+Digite 4 -> Franco(fr)\n" +
+                                  "+Digite 5 -> Iene(¥)\n");
+                Console.Write("R: ");
+                int op = int.Parse(Console.ReadLine());
+                if (op < 1 || op > 5)
+                {
+                    Console.WriteLine("Opção Inválida!");
+                }
+                else
+                {
+                    Console.WriteLine("*Digite seu valor:");
+                    if (op == 1)
+                    {
+                        Console.Write("R$ ");
+                        real = double.Parse(Console.ReadLine());
 
+                        dolar = real * 0.1975;
+                        libra = real * 0.16;
+                        franco = real * 0.18;
+                        iene = real * 26.22;
+                    }
+                    if (op == 2)
+                    {
+                        Console.Write("$ ");
+                        dolar = double.Parse(Console.ReadLine());
+
+                        real = dolar * 5.06;
+                        libra = dolar * 0.81;
+                        franco = dolar * 0.91;
+                        iene = dolar * 132.78;
+                    }
+                    if (op == 3)
+                    {
+                        Console.Write("£ ");
+                        libra = double.Parse(Console.ReadLine());
+
+                        dolar = libra * 1.2346;
+                        real = libra * 6.25;
+                        franco = libra * 1.13;
+                        iene = libra * 163.91;
+                    }
+                    if (op == 4)
+                    {
+                        Console.Write("fr ");
+                        franco = double.Parse(Console.ReadLine());
+
+                        dolar = franco * 1.0946;
+                        libra = franco * 0.89;
+                        real = franco * 5.53;
+                        iene = franco * 145.13;
+                    }
+                    if (op == 5)
+                    {
+                        Console.Write("¥ ");
+                        iene = double.Parse(Console.ReadLine());
+
+                        dolar = iene * 0.007575;
+                        libra = iene * 0.0062;
+                        franco = iene * 0.0069;
+                        real = iene * 0.038;
+                    }
+
+                    Console.WriteLine("-- Conversão --\n" +
+                        $"Real: R${real.ToString("N2")}\n" +
+                        $"Dólar: ${dolar.ToString("N2")}\n" +
+                        $"Libra: £{libra.ToString("N2")}\n" +
+                        $"Franco: fr{franco.ToString("N2")}\n" +
+                        $"Iene: ¥{iene.ToString("N2")}");
+                }
 
 
                 Console.WriteLine("*Deseja Refazer o exercício? (Sim,Nao)(S,N)");
@@ -179,12 +253,27 @@ namespace Exercicios
             com a exibição do resultado no programa principal.
              */
 
-            string continua = "";
+            string continua = "", resp = "";
             do
             {
                 Console.WriteLine("\n- Exercicio 04 - Capítulo 7\n");
 
+                Console.Write("Digite um Número: ");
+                double n1 = double.Parse(Console.ReadLine());
+                Console.Write("Digite um segundo Número: ");
+                double n2 = double.Parse(Console.ReadLine());
 
+                if (n1%n2 == 0 )
+                {
+                    resp += $"O numero {n1} é divisivel por {n2}\n";
+                }
+                else resp += $"O numero {n1} NÃO é divisivel por {n2}\n";
+
+                if (n2 % n1 == 0)
+                {
+                    resp += $"O numero {n2} é divisivel por {n1}";
+                }
+                else resp += $"O numero {n2} NÃO é divisivel por {n1}"; ;
 
                 Console.WriteLine("*Deseja Refazer o exercício? (Sim,Nao)(S,N)");
                 continua = Console.ReadLine().ToUpper();
@@ -193,8 +282,8 @@ namespace Exercicios
         public void Exercicio5()
         {
             /*
-             5.	Faça um algoritmo que possibilite o arredon- damento de um número real para um núme- ro inteiro, 
-            seguindo o arredondamento pa- dráo, utilizando a passagem por referência
+             5.	Faça um algoritmo que possibilite o arredondamento de um número real para um número inteiro, 
+            seguindo o arredondamento padrão, utilizando a passagem por referência
              */
 
             string continua = "";
@@ -202,7 +291,9 @@ namespace Exercicios
             {
                 Console.WriteLine("\n- Exercicio 05 - Capítulo 7\n");
 
-
+                Console.Write("Insira um número (ex: 2,7): ");
+                double number = double.Parse(Console.ReadLine());
+                Console.WriteLine($"\nNumero arredondado para inteiro: {number.ToString("N0")}");
 
                 Console.WriteLine("*Deseja Refazer o exercício? (Sim,Nao)(S,N)");
                 continua = Console.ReadLine().ToUpper();
@@ -231,8 +322,10 @@ namespace Exercicios
              Escreva uma função, para preencher um vetor, com 30 números inteiros sorteados 
             aleatoriamente. O vetor devera ser retornado ao programa principal, e vocé deve 
             acrescentar:
-            a)	Um procedimento que recebe o vetor preenchido no item anterior, faz a orde- nação dos elementos e apresenta o resul- tado através de uma mensagem exibida ao usuário.
-            b)	Uma funçáo que recebe o vetor preen- chido no item a e que retorna a quan- tidade de números múltiplos de 7 que existem no conjunto.
+            a)	Um procedimento que recebe o vetor preenchido no item anterior, faz a ordenação dos elementos e apresenta o resultado 
+            através de uma mensagem exibida ao usuário.
+            b)	Uma funçáo que recebe o vetor preenchido no item a e que retorna a quantidade de números 
+            múltiplos de 7 que existem no conjunto.
             c)	Escreva o programa principal e faça a chamada aos métodos criados nos itens a, b e c.
 
              */
@@ -240,7 +333,7 @@ namespace Exercicios
             string continua = "";
             do
             {
-                Console.WriteLine("\n- Exercicio 01 - Capítulo 7\n");
+                Console.WriteLine("\n- Exercicio 09 - Capítulo 7\n");
 
 
 
