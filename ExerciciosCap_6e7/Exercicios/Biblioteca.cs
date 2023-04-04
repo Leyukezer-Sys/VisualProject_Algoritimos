de2477bb4d7578e4c2691c7358203bd9483ee447
Leyukezer Cruz <51835032+Leyukezer-Sys@users.noreply.github.com>
2023-04-03 22:16:26 -0400

    Add files via upload

---
+++ ExerciciosCap_6e7/Exercicios/Biblioteca.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio6_2
+{
+    public class Biblioteca
+    {
+        public string DiaDaSemana(int n)
+        {
+            string dia = "";
+            if (n == 0)
+            {
+                dia = "Domingo      ";
+            }
+            else
+            if (n == 1)
+            {
+                dia = "Segunda-Feira";
+            }
+            else
+            if (n == 2)
+            {
+                dia = "Terça-Feira  ";
+            }
+            else
+            if (n == 3)
+            {
+                dia = "Quarta-Feira ";
+            }
+            else
+            if (n == 4)
+            {
+                dia = "Quinta-Feira ";
+            }
+            else
+            if (n == 5)
+            {
+                dia = "Sexta-Feira  ";
+            }
+            else
+            if (n == 6)
+            {
+                dia = "Sábado       ";
+            }
+            else
+            {
+                dia = "Indefinido";
+            }
+            return dia;
+        }
+        public int[] OrdernarVetor(int[] vetor, string forma)
+        {
+            if (forma == "CRE")
+            {
+                Array.Sort(vetor);
+            }
+            else if (forma == "DEC")
+            {
+                Array.Sort(vetor);
+                Array.Reverse(vetor);
+            }
+
+            return vetor;
+        }
+        public int[] OrdernarVetor(int[] vetor)
+        {
+
+            Array.Sort(vetor);
+
+            return vetor;
+        }
+        public int[] GerarMatrizMultiploN(int[] vetorExtracao, int tamanho, int multiploDe)
+        {
+            int indice = 0;
+            int[] vetorN = new int[tamanho];
+            
+            for (int i = 0; i < vetorExtracao.Length; i++)
+            {
+                if (vetorExtracao[i] % multiploDe == 0)
+                {
+                    vetorN[indice] = vetorExtracao[i];
+
+                    indice++;
+                }
+            }
+            return vetorN;
+        }
+        public double[] GerarMatrizMultiploN(double[] vetorExtracao, int tamanho, int multiploDe)
+        {
+            double[] vetorN = new double[tamanho];
+            int indice = 0;
+            for (int i = 0; i < vetorExtracao.Length; i++)
+            {
+                if (vetorExtracao[i] % multiploDe == 0)
+                {
+                    vetorN[indice] = vetorExtracao[i];
+                    indice++;
+                }
+            }
+            return vetorN;
+        }
+        public void ConversorMoeda(string moeda)
+        {
+            double real = 0.0, libra = 0.0, dolar = 0.0, franco = 0.0, iene = 0.0;
+            Console.WriteLine("*Digite seu valor:");
+            if (moeda == "R$")
+            {
+                Console.Write("R$ ");
+                real = double.Parse(Console.ReadLine());
+
+                dolar = real * 0.1975;
+                libra = real * 0.16;
+                franco = real * 0.18;
+                iene = real * 26.22;
+            }
+            else
+            if (moeda == "$")
+            {
+                Console.Write("$ ");
+                dolar = double.Parse(Console.ReadLine());
+
+                real = dolar * 5.06;
+                libra = dolar * 0.81;
+                franco = dolar * 0.91;
+                iene = dolar * 132.78;
+            }
+            else
+            if (moeda == "£")
+            {
+                Console.Write("£ ");
+                libra = double.Parse(Console.ReadLine());
+
+                dolar = libra * 1.2346;
+                real = libra * 6.25;
+                franco = libra * 1.13;
+                iene = libra * 163.91;
+            }
+            else
+            if (moeda == "fr")
+            {
+                Console.Write("fr ");
+                franco = double.Parse(Console.ReadLine());
+
+                dolar = franco * 1.0946;
+                libra = franco * 0.89;
+                real = franco * 5.53;
+                iene = franco * 145.13;
+            }
+            else
+            if (moeda == "¥")
+            {
+                Console.Write("¥ ");
+                iene = double.Parse(Console.ReadLine());
+
+                dolar = iene * 0.007575;
+                libra = iene * 0.0062;
+                franco = iene * 0.0069;
+                real = iene * 0.038;
+            }
+            else
+            {
+                Console.Write("R$ ");
+                real = double.Parse(Console.ReadLine());
+
+                dolar = real * 0.1975;
+                libra = real * 0.16;
+                franco = real * 0.18;
+                iene = real * 26.22;
+            }
+
+            Console.WriteLine("-- Conversão --\n" +
+                $"Real: R${real.ToString("N2")}\n" +
+                $"Dólar: ${dolar.ToString("N2")}\n" +
+                $"Libra: £{libra.ToString("N2")}\n" +
+                $"Franco: fr{franco.ToString("N2")}\n" +
+                $"Iene: ¥{iene.ToString("N2")}");
+        }
+        public int[] SortearValoresVetor(int tamanho)
+        {
+            int[] numbers = new int[tamanho];
+            Random sortear = new Random();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = sortear.Next(100);
+            }
+            return numbers;
+        }
+        public int ContadorMatrizMultiploN(int[] matrizExtracao, int multiplo)
+        {
+            int contador = 0;
+            for (int i = 0; i < matrizExtracao.Length; i++)
+            {
+                if (matrizExtracao[i] % multiplo == 0)
+                {
+                    contador++;
+                }
+            }
+            return contador;
+        }
+    }
+
+}
---
+++ ExerciciosCap_6e7/Exercicios/Program.cs
@@ -0,0 +1,75 @@
+﻿using Exercicios;
+
+string resposta = "S";
+Capitulo6 exer = new Capitulo6();
+Capitulo7 exerc = new Capitulo7();
+
+do
+{
+    Console.Clear();
+    Console.WriteLine("=========== EXERCICIOS CAPITULOS 6 E 7 ===========\n" +
+                      "- Índice Exercícios Capitulo 6:\n" +
+                      "+ Exercício 2 => Digite 1\n" +
+                      "+ Exercício 3 => Digite 2\n" +
+                      "+ Exercício 7 => Digite 3\n" +
+                      "+ Exercício 8 => Digite 4\n" +
+                      "+ Exercício 9 => Digite 5\n" +
+                      "- Índice Exercícios Capitulo 7:\n" +
+                      "+ Exercício 1 => Digite 6\n" +
+                      "+ Exercício 2 => Digite 7\n" +
+                      "+ Exercício 3 => Digite 8\n" +
+                      "+ Exercício 4 => Digite 9\n" +
+                      "+ Exercício 5 => Digite 10\n" +                      
+                      "+ Exercício 9 => Digite 11\n" +
+                      "- Índice Extra:\n" +
+                      "+ Exercício 6.10 => Digite 12");
+    Console.Write("Sua Escolha: ");
+    int op = int.Parse(Console.ReadLine());
+    switch (op)
+    {
+        case 1:
+            exer.Exercicio2();
+            break;
+        case 2:
+            exer.Exercicio3();
+            break;
+        case 3:
+            exer.Exercicio7();
+            break;
+        case 4:
+            exer.Exercicio8();
+            break;
+        case 5:
+            exer.Exercicio9();
+            break;
+        case 6:
+            exerc.Exercicio1();
+            break;
+        case 7:
+            exerc.Exercicio2();
+            break;
+        case 8:
+            exerc.Exercicio3();
+            break;
+        case 9:
+            exerc.Exercicio4();
+            break;
+        case 10:
+            exerc.Exercicio5();
+            break;
+        case 11:
+            exerc.Exercicio9();
+            break;
+        case 12:
+            exer.Exercicio10();
+            break;
+        default:
+            Console.WriteLine("Selecione uma opção Válida");
+            Console.ReadKey();
+            continue;
+    }
+    Console.WriteLine("Deseja Realizar Outro Exercício? (Sim,Nao)(S,N)");
+    resposta = Console.ReadLine().ToUpper();
+} while (resposta.Equals("SIM") || resposta.Equals("S"));
+
+
