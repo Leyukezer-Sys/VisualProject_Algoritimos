38ea21a13049c2f3ec5a90ed4f15e0f3cdd444db
Leyukezer Cruz <51835032+Leyukezer-Sys@users.noreply.github.com>
2023-05-08 22:12:26 -0400

    Add files via upload

---
+++ Aula08052023/ExemploListas/ExemploListas/Aluno.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploListas
+{
+    public class Aluno
+    {
+        //1º Criar classe
+        //2º mudar para 'public class' o 'internal class'
+        //3º Colocar os atributos
+        public string? nome, cpf;
+        public double[]? notas;
+        public double media;
+        //4º criar o ponteiro, mesmo nome da classe no tipo da variavel
+        public Aluno prox;
+    }
+}
---
+++ Aula08052023/ExemploListas/ExemploListas/Program.cs
@@ -0,0 +1,120 @@
+﻿using ExemploListas;
+using System.Diagnostics;
+
+//5º colocar namespace
+//6º criar os elementos da lista
+Aluno novo = null, inicio = null, lista = null, aux = null;
+int op = 0;
+string cond = "";
+//7º inserir os dados dos elementos
+do
+{
+
+    Console.WriteLine("** Escolha Sua Opção:\n" +
+        "   1 - Para Cadastrar\n" +
+        "   2 - Para Listar Todos\n" +
+        "   3 - Para Consultar um Aluno\n" +
+        "   0 - Sair");
+    Console.Write("R: ");
+    op = int.Parse(Console.ReadLine());
+    switch (op)
+    {
+        case 1:
+            //8º Novo elemento
+            novo = new Aluno();
+
+            Console.Write("\nDigite o nome do aluno: ");
+            novo.nome = Console.ReadLine();
+            Console.Write("Digite o cpf do aluno: ");
+            novo.cpf = Console.ReadLine();
+            Console.Write("Digite o nota do aluno: ");
+            novo.notas = new double[1];
+            novo.notas[0] = double.Parse(Console.ReadLine());
+
+            //9 encadeamento de lista
+            //  9.1 colocar no inicio da lista
+            if (inicio == null)
+            {
+                inicio = novo;
+                inicio.prox = null;
+            }
+            //  9.2 colocar no 2º nó
+            else
+            {
+                if (inicio.prox == null)
+                {
+                    lista = novo;
+                    inicio.prox = lista;
+                    lista.prox = null;
+                }
+
+                lista.prox = novo;
+                lista = novo;
+                lista.prox = null;
+            }
+            break;
+        case 2:
+            //10º consultar lista
+            aux = inicio;
+
+            //11ºpercorrer a lista
+            while (aux != null)
+            {
+                Console.WriteLine($"Notas do Aluno(a) {aux.nome}|{aux.cpf}: {aux.notas[0]}");
+                aux = aux.prox;
+            }
+            break;
+        case 3:
+            Console.WriteLine("** Escolha:\n" +
+        "   1 - Para Pesquisar Nome\n" +
+        "   2 - Para Pesquisar CPF");
+            Console.Write("R: ");
+            int pe = int.Parse(Console.ReadLine());
+            if (pe == 1)
+            {
+                Console.WriteLine("\nPesquise o nome do aluno(a): ");
+                string pesq = Console.ReadLine();
+
+                aux = inicio;
+
+                while (aux != null)
+                {
+                    if (aux.nome == pesq)
+                    {
+                        Console.WriteLine($"-- Notas do Aluno(a) {aux.nome}|{aux.cpf}: {aux.notas[0]} --");
+                        break;
+                    }
+                    aux = aux.prox;
+                }
+            }
+            if (pe == 2)
+            {
+                Console.WriteLine("\nPesquise o CPF do aluno(a): ");
+                string pesq = Console.ReadLine();
+
+                aux = inicio;
+
+                while (aux != null)
+                {
+                    if (aux.cpf == pesq)
+                    {
+                        Console.WriteLine($"\nNotas do Aluno(a) {aux.nome}|{aux.cpf}: {aux.notas[0]}");
+                        break;
+                    }
+                    aux = aux.prox;
+                }
+            }
+
+            break;
+    }
+    if (op != 0)
+    {
+        Console.WriteLine("\n\nDeseja voltar pro menu? (s/n)(sim/nao)");
+        cond = Console.ReadLine().ToUpper();
+        if (cond.Equals("S") || cond.Equals("SIM"))
+        {
+            Console.Clear();
+        }
+    }
+    
+} while (cond.Equals("S")|| cond.Equals("SIM"));
